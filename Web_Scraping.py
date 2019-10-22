830f06fab59efb8932aaa2db3dba13258e6f7450
irenemdes <irene.mdes@gmail.com>
2019-10-22 21:36:25 +0100

    - Scraping of the "Novedades" resource
    Petición a la página de novedades y parseo de los datos para obtener últimos títulos y URL asociada a cada uno.

--- Web_Scraping.py
+++ Web_Scraping.py
@@ -1,22 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_HTML(url="https://api.github.com"):
+URL = "http://quelibroleo.hola.com/novedades"
+
+def get_HTML(url):
     returnValue = None
     response = requests.get(url)
     if response.status_code == 200:
         returnValue = response.content
     return returnValue
 
+def get_latest_releases(url):
+    returnValue = None
+    results = []
+    HTML = get_HTML(URL)
+    if HTML:
+        soup = BeautifulSoup(HTML, 'html.parser')
+        #print(soup.prettify())
+        items = soup.find_all("div", class_="item")
+
+        for item in items:
+            item_info = item.find("div", class_ = "col-lg-8 col-xs-12")
+            #print (item_info)
+            title = item_info.a.string
+            book_url = item_info.a.get('href')
+            json_info = {"Titulo":title, "URL": book_url}
+            results.append(json_info)
+
+        returnValue = results
+    return returnValue
+
+    latest_releases = get_latest_releases(URL)
 
 
-URL = "http://quelibroleo.hola.com/novedades"
-HTML = get_HTML(URL)
-if HTML:
-    soup = BeautifulSoup(HTML, 'html.parser')
-    #print(soup.prettify())
-    items = soup.find_all("div", class_="item")
-
-    for item in items:
-        item_info = item.find("div", class_ = "col-lg-8 col-xs-12")
-        print (item_info)
