18098e6af54a992d904f9dcb8b33fe496f883285
irenemdes <irene.mdes@gmail.com>
2019-10-24 20:19:30 +0100

    - Full Scraping of book information
    Se incluye toda la información de cada libro incluido en el apartado de novedades en el modelo de datos.

--- Web_Scraping.py
+++ Web_Scraping.py
@@ -13,9 +13,9 @@ def get_HTML(url):
 def get_latest_releases(url):
     returnValue = None
     results = []
-    HTML = get_HTML(URL)
-    if HTML:
-        soup = BeautifulSoup(HTML, 'html.parser')
+    html_content = get_HTML(url)
+    if html_content:
+        soup = BeautifulSoup(html_content, 'html.parser')
         #print(soup.prettify())
         items = soup.find_all("div", class_="item")
 
@@ -30,6 +30,35 @@ def get_latest_releases(url):
         returnValue = results
     return returnValue
 
-    latest_releases = get_latest_releases(URL)
+def get_all_book_info (book_url):
+    returnValue = {}
+    html_content = get_HTML(book_url)
+    soup = BeautifulSoup(html_content, 'html.parser')
+    book_info = soup.find("div", class_="libro_info")
+    author = book_info.div.h3.small.string
+    average_score = book_info.find("div", class_="estadisticas").span.string
+    returnValue["Autor"] = author
+    returnValue["Puntuacion"] = average_score
 
+    card_info = book_info.find("div", class_="card")
+    card_objects = card_info.find("li")
+    first_card_object = card_objects.find("span")
+    first_card_value = card_objects.find("a")
+    returnValue[first_card_object.string] = first_card_value.string
+    for nextSibling in card_objects.findNextSiblings():
+        try:
+            returnValue[nextSibling.find("span").string] = nextSibling.find("a").string
+        except:
+            aux_string = str(nextSibling)
+            index_label = aux_string.find('</span>')+len('</span>')
+            value = (aux_string[index_label:]).replace("</li>","").strip()
+            returnValue[nextSibling.span.string] = value
+    return returnValue
 
+latest_releases = get_latest_releases(URL)
+print (latest_releases)
+for index, book in enumerate(latest_releases,start=0):
+   book_info = get_all_book_info(book["URL"])
+   latest_releases[index].update(book_info)
+print(latest_releases)
+#print (book_info)
